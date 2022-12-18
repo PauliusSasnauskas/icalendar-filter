ab242994ac84a5c46dc3b747fa467a5f13b7e3ea
Paulius Sasnauskas <40543380+PauliusSasnauskas@users.noreply.github.com>
2022-12-18 11:17:52 +0000

    Keep original calendar name

--- ical-filter.ts
+++ ical-filter.ts
@@ -13,7 +13,9 @@ function isValid (checkString: string, bads: string[]): boolean {
 export async function icalFilter (sourceURL: string, titleFilters: string[], yearFilters: string[] = []) {
   const cal = await icalParse.fromURL(sourceURL)
 
-  const newCal = icalCalendar({ name: 'ICL Calendar Feed' })
+  const title = (cal['vcalendar'] as any)?.['WR-CALNAME'] ?? 'iCalendar'
+
+  const newCal = icalCalendar({ name: `${title} Filtered`, prodId: { company: 'Paulius Sasnauskas', product: 'iCalendar Filter' } })
 
   for (const key in cal) {
     const item = cal[key]
